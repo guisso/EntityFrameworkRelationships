841b728c69efa4d359d24ac4fbea02abfa6097a3
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-20 11:28:44 -0300

    Basic Endereco

---
+++ Endereco.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkRelationships
+{
+    public class Endereco
+    {
+        public UInt64 Id { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        [MaxLength(15)]
+        public String? TipoLogradouro { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        [MaxLength(120)]
+        public String? Logradouro { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        public UInt16? Numero { get; set; }
+        
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        [MaxLength(50)]
+        public String? Bairro { get; set; }
+        
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        public UInt32? Cep { get; set; }
+        
+    }
+}
