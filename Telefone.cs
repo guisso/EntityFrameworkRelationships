852e30fd0d8b97d9b77571cd1cabe4f425e632dd
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-20 11:21:51 -0300

    Basic Telefone

---
+++ Telefone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkRelationships
+{
+    public class Telefone
+    {
+        public UInt64 Id { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        public Byte Ddd { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        public UInt32 Numero { get; set; }
+    }
+}
