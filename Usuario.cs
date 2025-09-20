0b2cdb0b567bb5ac3a7428e8e400c4a2d6d610b5
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-20 11:05:17 -0300

    Basic Usuario

---
+++ Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkRelationships
+{
+    public class Usuario
+    {
+        public Int64 Id { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        [MaxLength(45)]
+        public String? Nome { get; set; }
+
+        #region ToString
+        public override String ToString()
+        {
+            return Id
+                + ", " + Nome;
+        }
+        #endregion
+    }
+}
