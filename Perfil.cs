ed28f908016d0051d1b6155495f12b24e9ef3781
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-20 11:35:06 -0300

    Basic Perfil

---
+++ Perfil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkRelationships
+{
+    [Index(nameof(Tipo), IsUnique = true)]
+    public class Perfil
+    {
+        public UInt64 Id { get; set; }
+
+        [Required]
+        public Char? Tipo { get; set; }
+
+        public Boolean? Ler { get; set; }
+
+        public Boolean? Escrever { get; set; }
+
+        public Boolean? Excluir { get; set; }
+    }
+}
