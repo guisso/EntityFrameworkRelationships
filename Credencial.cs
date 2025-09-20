e3c53b4a500d446d484ce9b2fb7b4a48f1b12912
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-20 11:13:28 -0300

    Basic Credencial

---
+++ Credencial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkRelationships
+{
+    public class Credencial
+    {
+        public UInt64 Id { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        [MaxLength(200)]
+        [EmailAddress]
+        public String? Email { get; set; }
+
+        // TODO : Implement constraints in the UML class diagram
+        [Required]
+        [MinLength(8)]
+        [MaxLength(20)]
+        public String? Senha { get; set; }
+    }
+}
