40448eaf44f82b53fc9a30c3f1692969d4f77ccd
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-20 10:56:49 -0300

    Initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkRelationships
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("EntityFrameworkRelationships running");
+        }
+    }
+}
